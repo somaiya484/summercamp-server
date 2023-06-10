b56c7a9553d834d5c3fe7bf7804c32c93b983930
somaiya shahin <somaiyashahin484@gmail.com>
2023-06-11 01:43:23 +0600

    deleted added

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jd8zyik.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jd8zyik.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,25 +29,36 @@ async function run() {
     const instructorCollection = client.db("summerCamp").collection("instructor");
     const classCollection = client.db("summerCamp").collection("class");
 
-    app.get('/instructor', async(req, res) =>{
+    app.get('/instructor', async (req, res) => {
       const result = await instructorCollection.find().toArray();
       res.send(result)
     })
 
-    app.post('/class', async(req, res) =>{
+    // class collection APIS
+    app.get('/class', async (req, res) => {
+      const email = req.query.email;
+      console.log(email)
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await classCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.post('/class', async (req, res) => {
       const clas = req.body;
       console.log(clas);
       const result = await classCollection.insertOne(clas);
       res.send(result);
     })
 
-
-
-
-
-
-
-
+    app.delete('/class/:id', async( req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await classCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
