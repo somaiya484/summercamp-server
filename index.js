63908c49783976f48a03644ff1662d6ad0077ad2
somaiya shahin <somaiyashahin484@gmail.com>
2023-06-11 11:36:01 +0600

    patch admin state

--- index.js
+++ index.js
@@ -47,6 +47,18 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await allUsersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
     app.get('/instructor', async (req, res) => {
       const result = await instructorCollection.find().toArray();
       res.send(result)
